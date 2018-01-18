30672d527f6c6edf63717d2edcd91b352b36f7ef
MrDwina <diwina1993@hotmail.com>
2018-01-18 10:02:31 +0000

    created cotact, about-us and galary pages

--- client/routes/index.js
+++ client/routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Cyber Group' });
 });
 
+/* GET Galary page. */
+router.get('/galary', function (req, res, next) {
+  res.render('galary', { title: "Cyber Group's galary" });
+});
+
+/* GET Contact page. */
+router.get('/contact', function (req, res, next) {
+  res.render('contact', {title: "Cyber Group's contact" });
+});
+
+/* GET About us page. */
+router.get('/about-us', function (req, res, next) {
+  res.render('about-us', { title: "Cyber Group's about us page" });
+});
+
 module.exports = router;
