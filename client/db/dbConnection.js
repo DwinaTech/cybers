5911fa88613ef6fc9059905aca6957e0b271d127
MrDwina <diwina1993@hotmail.com>
2018-01-21 21:33:05 +0000

    edited general design

--- client/db/dbConnection.js
+++ client/db/dbConnection.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 mongoose.Promise = global.Promise;
 
-const promise = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/cybergroup', {
+const promise = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/cyber', {
     useMongoClient: true
 });
--- client/routes/index.js
+++ client/routes/index.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const router = express.Router();
+require('../db/dbConnection')
 const Feedback = require('../models/Feedback');
-const {ObjectId} = require('mongoose')
+const mongoose = require('mongoose')
+const ObjectId = require('mongodb').ObjectID;
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index',
-    {
-      title: 'Cyber Group',
-    });
-});
+router.get('/', function (req, res, next) {
+  Feedback.find({}, (err, feedbackData) => {
+    res.render('index', {
+      feedbackData
+    })
+  });
+});  
 
 /* Post feedback data page. */
 router.post('/', function (req, res, next) {
   Feedback.create(req.body).then(data => {
-    res.redirect('index');
+    res.redirect('/');
   });
 });
 
