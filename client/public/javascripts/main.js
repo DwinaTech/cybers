f204297453ea548654f7760d3a79d08d1afb9f66
MrDwina <diwina1993@hotmail.com>
2018-01-22 15:45:56 +0000

    stoped using DB for short timee

--- client/public/javascripts/main.js
+++ client/public/javascripts/main.js
@@ -1,13 +1,13 @@
-const mainList = document.getElementById('mainList');
-const mainMenu = document.getElementById('mainMenu');
+// const mainList = document.getElementById('mainList');
+// const mainMenu = document.getElementById('mainMenu');
 
-mainMenu.addEventListener('click', () => {
-    if (mainList.style.display != 'block') {
-        mainList.style.marginTop = '-.1em'
-        mainList.style.display = 'block'
-    } else {
-        mainList.style.marginTop = '-11em'
-        mainList.style.transition = 'all .5s ease-in-out'
-        mainList.style.display = 'none'
-    }
-})
+// mainMenu.addEventListener('click', () => {
+//     if (mainList.style.display != 'block') {
+//         mainList.style.marginTop = '-.1em'
+//         mainList.style.display = 'block'
+//     } else {
+//         mainList.style.marginTop = '-11em'
+//         mainList.style.transition = 'all .5s ease-in-out'
+//         mainList.style.display = 'none'
+//     }
+// })
--- client/routes/index.js
+++ client/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-require('../db/dbConnection')
+// require('../db/dbConnection')
 const Feedback = require('../models/Feedback');
 const mongoose = require('mongoose')
 const ObjectId = require('mongodb').ObjectID;
@@ -8,11 +8,10 @@ const ObjectId = require('mongodb').ObjectID;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  Feedback.find({}, (err, feedbackData) => {
-    res.render('index', {
-      feedbackData
-    })
-  });
+  // Feedback.find({}, (err, feedbackData) => {
+    
+  // });
+  res.render('index')
 });  
 
 /* Post feedback data page. */
@@ -35,6 +34,35 @@ router.get('/contact', function (req, res, next) {
 /* GET About us page. */
 router.get('/about-us', function (req, res, next) {
   res.render('about-us', { title: "Cyber Group's about us page" });
+});
+
+/* Delete page. */
+router.get('/delete/:feedbackId', function (req, res, next) {
+  const {feedbackId} = req.params;
+  Feedback.findByIdAndRemove(feedbackId, (err) => {
+    if (err) {
+      res.json(err.message)
+    } else {
+      res.redirect('/');
+    }
+  })
+});
+
+/* Update page. */
+router.get('/edit/:feedbackId', function (req, res, next) {
+  const feedbackId  = req.params.feedbackId;
+  Feedback.findById(feedbackId).exec((err, editFeedback) => {
+        res.redirect('/', {
+          editFeedback
+        });
+      }).catch(next)
+});
+
+router.post('/edit/:feedbackId', function (req, res, next) {
+  const { feedbackId } = req.params;
+  Feedback.findByIdAndUpdate(feedbackId, req.body, (editFeedback) => {
+    res.render('/');
+  })
 });
 
 module.exports = router;
